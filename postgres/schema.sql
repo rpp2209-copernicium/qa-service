a3aabb012ab9af681f20a2e9f23a71055e3193fa
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-03-02 21:51:14 -0500

    Build command now successfully executes the schema.sql file, creates the qa database + tables,and inserts a test row in each table (questions, answers, and answers_photos). Confirmed by running docker exec <container> and using psql to query the inserted rows.

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -4,6 +4,7 @@ BEGIN;
 
 CREATE SCHEMA qa;
 
+-- QUESTIONS TABLE
 CREATE TABLE questions(
 	id INT PRIMARY KEY NOT NULL,
 	product VARCHAR(255) NOT NULL,
@@ -30,4 +31,56 @@ INSERT INTO questions(
 	2
 );
 
+SAVEPOINT q_db_created;
+
+-- ANSWERS TABLE
+CREATE TABLE answers(
+	id INT PRIMARY KEY NOT NULL,
+	question INT REFERENCES questions NOT NULL,
+	body VARCHAR(255),
+	created_at TIMESTAMP,
+	answerer_name VARCHAR(255) NOT NULL,
+	reported BOOLEAN,
+	helpful INT
+);
+
+INSERT INTO answers(
+	id,
+	question,
+	body,
+	answerer_name,
+	reported,
+	helpful
+) VALUES (
+	1,
+	1,
+	'100% Cotton',
+	'billybob',
+	false,
+	5
+);
+
+SAVEPOINT a_db_created;
+
+-- PHOTOS TABLE
+CREATE TABLE answers_photos(
+	id INT PRIMARY KEY NOT NULL,
+	answer INT REFERENCES answers NOT NULL,
+	url VARCHAR(255)
+);
+
+INSERT INTO answers_photos(id, answer, url) VALUES (1, 1, 'default.png');
+
+SAVEPOINT p_db_created;
+
+	-- CREATE A VIEW
+	-- Possible views: question_answers
+	-- CREATE VIEW get_one_question AS
+  --       SELECT id, body, asker_name
+  --       FROM questions
+  --       LIMIT 1;
+	-- CREATE VIEW helpful_questions AS
+	-- 			SELECT id, body, asker_name, helpful
+	-- 			FROM questions
+	-- 			WHERE helpful > 0;
 COMMIT;
