1a46f0d2bca292ab846de30244d8459076c23428
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-03-15 17:53:33 -0400

    Docker Config complete. Running ` docker-compose up -d --build` now creates the Postgres Container + Volumes + builds all Images, creates the qa database + associated tables, and copies all CSV data into the newly created db (via shell script / without a manual call to `psql \copy` from inside the running container).
    TO-DO: Link containers and optimize query speed. Replace legacy API.

--- postgres/scripts/setup-db.sh
+++ postgres/scripts/setup-db.sh
@@ -1,51 +1,18 @@
 #! /bin/bash
 
-# ------ TAGS ------
-# -d Run command in detached mode
-# -it Interactive mode (?)
-# -c Execute the following command
-
 POSTGRES_USER=admin
 POSTGRES_PASSWORD=sdc
 POSTGRES_DB=qa
-# `admin` cli argument is the username for the database
 
 set -e
 
+# Populate the questions, answers, and answers_photos database tables
+
+# `admin` cli argument is the username for the database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	\c qa;
 	\dt;
+	\copy questions( id, product_id, body, date_written, asker_name, asker_email, reported, helpful) FROM '/usr/tmp/data/questions.csv' DELIMITER ',' CSV HEADER;
+	\copy answers( id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) FROM 'answers.csv' DELIMITER ',' CSV HEADER;
+	\copy answers_photos( id, answer_id, url) FROM 'answers_photos.csv' DELIMITER ',' CSV HEADER;
 EOSQL
-
-# populate the questions database table
-# psql  -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-# 	COPY questions(
-# 		id,
-# 		product_id,
-# 		body,
-# 		date_written,
-# 		asker_name,
-# 		asker_email,
-# 		reported,
-# 		helpful
-# 	) FROM 'questions.csv' DELIMITER ',' CSV HEADER;
-# EOSQL
-
-# Populate the Answers database table
-# psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "\COPY answers(
-# 	id,
-# 	question_id,
-# 	body,
-# 	date_written,
-# 	answerer_name,
-# 	answerer_email,
-# 	reported,
-# 	helpful
-# ) FROM 'answers.csv' DELIMITER ',' CSV HEADER;"
-
-# # Populate the Answers Photos database table
-# psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "\COPY answers_photos(
-# 	id,
-# 	answer_id,
-# 	url
-# ) FROM 'answers_photos.csv' DELIMITER ',' CSV HEADER;"
