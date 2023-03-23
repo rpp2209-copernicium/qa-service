4dd5192adb261390cadf5eda669a8342e0f49e30
Gabriela <48141726+gbmyt@users.noreply.github.com>
2023-03-23 01:29:56 -0400

    Questions (with and without product-id arg) data returns in the right shape.
    Answers data returns in the right shape also-- missing
    question_id, page, and count (todo)

--- api/utils/db.js
+++ api/utils/db.js
@@ -16,29 +16,24 @@ let dbString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`
 // =============================================
 let pgPool = new Pool({ connectionString: dbString });
 
-// todo:
-// convert this from jest syntax (?)
-// afterAll(async () => {
-//   await pgPool.end();
-// });
-
+// GET REQUESTS
 let fetch = async (endpoint, cb) => {
 	const client = await pgPool.connect();
 	let id, path, table;
 
-	// Check if the endpoint is /question_id/answers
-	// and do some variable setting if so
+	// Check the endpoint and do some variable setting based on its value
 	const regex = /^\d+\/answers$/;
 	const pathRegex = /^(.*)\?product_id=(\d+)$/;
-	console.log('match????', endpoint, endpoint.match(pathRegex));
+	console.log('REGEX MATCHED: ', endpoint, endpoint.match(pathRegex));
 
+	// IF Question ID was provided, grab it
 	if (endpoint.match(regex)) {
 		console.log('Match found', endpoint.match(regex));
 		id = endpoint.split('/')[0];
 		table = endpoint.split('/')[1];
-		console.log('Splitting on backslash-- id is:', id, 'table name is: ', table);
+		console.log('Splitting on backslash-- \nid is:', id, '\ntable name is: ', table);
 
-	} else if (endpoint.match(pathRegex)) {
+	} else if (endpoint.match(pathRegex)) { // ELSE, IF path provided, grab it instead
 
 		console.log('Match found', endpoint.match(pathRegex));
 		table = endpoint.split('/?product_id=')[0];
@@ -46,22 +41,24 @@ let fetch = async (endpoint, cb) => {
 		console.log('Splitting on Product ID-- path is:', path, 'table name is: ', table);
 	}
 
-  // const otherQuery = `
-	// 	SELECT answers.answer_id, JSON_AGG(
-	// 		json_build_object(
-	// 			'url', url,
-	// 			'answer_id', answers.answer_id
-	// 		)
-	// 	) photos FROM answers
-
-	// 	JOIN answers_photos ON answers_photos.answer_id=answers.answer_id
-	// 	GROUP BY answers.answer_id
-
-	// 	LIMIT 10
-	// `;
-
-	//WHERE answers_photos.answer_id=answers.answer_id
-	const qQuery = `
+	// =============================================
+	//           Build up the Queries...
+	// =============================================
+	// ANSWERS QUERY STRING
+	const aQuery = `
+		SELECT json_agg(
+			json_build_object(
+				'answer_id', answer_id,
+				'body', answer_body,
+				'date', answer_date,
+				'answerer_name', answerer_name,
+				'answer_helpfulness', answer_helpfulness,
+				'photos', json_build_array('test_value.png', 'value_the_second.png')
+			)
+		) results FROM answers WHERE ${ !id ? '' : `answers.question_id=${id}`}
+	`;
+	// ✅ QUESTIONS QUERY STRING
+	const qIDQuery = `
 		SELECT results.product_id, JSON_AGG(results) results FROM (
 
 			SELECT DISTINCT on (questions.question_id) questions.question_id, questions.product_id, questions.question_body, questions.question_date, questions.asker_name, questions.question_helpfulness, questions.reported,
@@ -87,21 +84,37 @@ let fetch = async (endpoint, cb) => {
 		GROUP BY results.product_id
 	`;
 
+	// const photosQuery = `
+	// 	SELECT answers.answer_id, JSON_AGG(
+	// 		json_build_object(
+	// 			'url', url,
+	// 			'answer_id', answers.answer_id
+	// 		)
+	// 	) photos FROM answers
+
+	// 	JOIN answers_photos ON answers_photos.answer_id=answers.answer_id
+	// 	GROUP BY answers.answer_id
+
+	// 	LIMIT 10
+	// `;
+
+	// =============================================
+	//          FINAL/AGGREGATED Q STRINGS
+	// =============================================
 	const qString = ((table === 'answers') ?
-		'SELECT questions.question_id, questions.product_id, questions.question_body, questions.question_date, questions.asker_name, questions.question_helpfulness, questions.reported'
-		: `${qQuery}`
+		`${aQuery}`
+		: `${ !path ? 'SELECT * FROM questions' : `${qIDQuery}`}`
 	);
 
-	const query = `
-		${qString}
-		${ table === 'answers' ? `GROUP BY product_id` : '' }
-		LIMIT 5
-	`;
+	const query = `${qString} LIMIT 15`;
 
+	console.log('Final Query String was: ', query);
+
+	// Finally, execute the query and send back the results
 	try {
 		const { rows } = await client.query(query);
-		cb(null, rows[0])
-		// cb(null, (table === 'answers' ? rows[0] : rows));
+		const result = (!path && table !== 'answers') ? rows : rows[0];
+		cb(null, result);
 	} catch (err) {
 		cb(err);
 	} finally {
@@ -109,6 +122,7 @@ let fetch = async (endpoint, cb) => {
 	}
 };
 
+// PUT REQUESTS
 let update = (endpoint, cb) => {
 	// todo
 	try {
@@ -120,6 +134,7 @@ let update = (endpoint, cb) => {
 
 };
 
+// POSTS REQUESTS
 let save = async (table, question, cb) => {
 	const client = await pgPool.connect();
 	const { product_id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported } = question;
