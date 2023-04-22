c39c462cefe36105de21ae766151292db959e02d
Gabriela Taylor <gabriela@Gabrielas-iMac.lan>
2023-04-22 11:35:35 -0400

    Added separate Questions and Answers stress scenarios in (local/K6) stress-test logic.

--- api/__tests__/stress-test.js
+++ api/__tests__/stress-test.js
@@ -3,39 +3,6 @@ import { check, sleep } from 'k6'; // sleep takes arg in seconds only (no ms)
 
 http.setResponseCallback(http.expectedStatuses({ min: 200, max: 300 }));
 
-// Smoke Test - Test Minimal Load
-// export const smokeTest_options = {
-//   stages: [
-//     { duration: '30s', target: 20 },
-//     { duration: '1m30s', target: 10 },
-//     { duration: '20s', target: 0 },
-//   ],
-// };
-
-// Performance Test - Test Normal Load
-// export const loadTest_options = {
-//   stages: [
-//     { duration: '30s', target: 20 },
-//     { duration: '1m30s', target: 10 },
-//     { duration: '20s', target: 0 },
-//   ],
-// };
-
-// Stress Test - Test under Extreme Conditions
-// export const options = {
-//   vus: 10,
-//   duration: '30s'
-// }
-
-// Soak Test - Test Reliability Over a Longer Period of Time
-// export const soakTest_options = {
-//   stages: [
-//     { duration: '30s', target: 20 },
-//     { duration: '1m30s', target: 10 },
-//     { duration: '20s', target: 0 },
-//   ],
-// };
-
 export const options = {
   discardResponseBodies: true,
 
@@ -47,68 +14,81 @@ export const options = {
 
   scenarios: {
 
-    stress: {
+    qStress: {
       executor: 'ramping-arrival-rate',
-      startRate: 1,
+
+      startRate: 10,
       timeUnit: '1s',
-      preAllocatedVUs: 60,
+      gracefulStop: '60s',
 
+      // GET Q's Config
+      preAllocatedVUs: 325,
+      maxVUs: 500,
       stages: [
-        { duration: "10s", target: 1  },
-        { duration: "20s", target: 5  },
-        { duration: "10s", target: 1  },
-        { duration: "20s", target: 0 }, // scale down. Recovery stage.
-      ],
+        { duration: "5s", target: 10  },
 
+        { duration: "5s", target: 100 },
+        { duration: "10s", target: 250 },
+
+        { duration: "5s", target: 450  },
+        { duration: "10s", target: 750 },
+        { duration: "5s", target: 450 },
+
+        { duration: "5s", target: 250  },
+        { duration: "10s", target: 100 },
+
+        { duration: "5s", target: 10 },
+        { duration: "10s", target: 0 }, // scale down. Recovery stage.
+      ],
     },
   },
 
-};
+  aStress: {
+    executor: 'ramping-arrival-rate',
 
-// QA Service API
-// Load Test QA Perfomance Locally (No Nginx, No EC2, No Legacy Endpoints)
-export default function () {
-  
-  const BASE_URL = 'http://localhost:8080/qa/questions';
+    startRate: 10,
+    timeUnit: '1s',
+    gracefulStop: '60s',
+    preAllocatedVUs: 375,
+    maxVUs: 500,
+    stages: [
+      { duration: "5s", target: 10  },
 
-  // Generate a random number between min and max:
-  // Math.random() * (max - min) + min
-  let min = 99999;
-  let max = 999999;
-  let id = Math.floor(Math.random() * (max - min) + min);
-  let res = http.get(BASE_URL + `?product_id=${id}`);
+      { duration: "5s", target: 600 },
+      { duration: "10s", target: 1100 },
 
-  console.log(`Response Time: ${String(res.timings.duration)} ms \n| URL was: ${String(res.url)}`);
-    
-    // for (let i = 99999; i <= 999999; id++) {
-    //   res = http.get(BASE_URL + `?product_id=${id}`);
+      { duration: "5s", target: 1250  },
+      { duration: "10s", target: 1510 },
+      { duration: "5s", target: 1250 },
 
-    //   console.log(`Response Time: ${String(res.timings.duration)} ms
-    //     \n| Errors: ${String(res.error)}
-    //     \n| URL was: ${String(res.url)}
-    //     \n| Status Text: ${String(res.status_text)}
-    //   `);
-    // }
+      { duration: "5s", target: 1100  },
+      { duration: "10s", target: 600 },
+
+      { duration: "5s", target: 10 },
+      { duration: "10s", target: 0 }, // scale down. Recovery stage.
+    ],
+  },
 
 }
 
-// =============================================
-//            Response Object Keys
-// =============================================
-// remote_ip,
-// remote_port,
-// url,
-// status,
-// status_text,
-// proto,
-// headers,
-// cookies,
-// body, 
-// timings, 
-// tls_version, 
-// tls_cipher_suite,
-// ocsp,
-// error,
-// error_code,
-// request,
-// getCtx
+export default function () {
+  const BASE_URL = 'http://localhost:8080/qa/questions';
+
+  // Generate a random number between min and max:
+
+  // GET QUESTIONS min/max
+  // let min = 99999;
+  // let max = 999999;
+
+  // GET ANSWERS min/max
+  let min = 351896;
+  let max = 3518963;
+  let id = Math.floor(Math.random() * (max - min) + min);
+
+  // TEST GET QUESTIONS
+  // let res = http.get(BASE_URL + `?product_id=${id}`);
+
+  // TEST GET ANSWERS
+  let res = http.get(BASE_URL + `/${id}/answers`);
+  //console.log(`Response Time: ${String(res.timings.duration)} ms \n| URL was: ${String(res.url)}`);
+}
--- api/server/index.js
+++ api/server/index.js
@@ -1,17 +1,15 @@
+// Comment New Relic OUT while stress-testing
+// const newrelic = require("newrelic");
+
 const http = require('http');
 const path = require("path");
 const express = require('express')
 
 const app = express();
-const app2 = express();
+//const app2 = express();
 
-// const ports = [8080, 8081, 8082];
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 const routes = require('./routes/questions.js');
-
-// =============================================
-//    REMOVE THIS WHEN EC2 FULLY DEBUGGED
-// =============================================
 const { fetch } = require('../utils/db.js');
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,28 +19,28 @@ app.get('/', (req, res) => {
   res.send('Hello 8080');
 });
 
-app2.get('/', (req, res) => {
-  res.send('Hello 8081');
-});
+//app2.get('/', (req, res) => {
+//  res.send('Hello 8081');
+//});
 
 // 8080 Loader.io Verification
-app.get('/loaderio-f7093aa72c81947cfb37405b167a9022.txt', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'loaderio-f7093aa72c81947cfb37405b167a9022.txt')); 
-});
+//app.get('/loaderio-f7093aa72c81947cfb37405b167a9022.txt', (req, res) => {
+//  res.sendFile(path.resolve(__dirname, 'loaderio-f7093aa72c81947cfb37405b167a9022.txt')); 
+//});
 
 // 8081 Loader.io Verification
-app2.get('/loaderio-f7093aa72c81947cfb37405b167a9022.txt', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'loaderio-f7093aa72c81947cfb37405b167a9022.txt')); 
-});
+//app2.get('/loaderio-f7093aa72c81947cfb37405b167a9022.txt', (req, res) => {
+//  res.sendFile(path.resolve(__dirname, 'loaderio-f7093aa72c81947cfb37405b167a9022.txt')); 
+//});
 
 app.use('/qa', routes);
-app2.use('/qa', routes);
+//app2.use('/qa', routes);
 
 app.listen(8080, () => {
   console.log(`Server running on port 8080`);
 });
 
-app2.listen(8081, () => {
-  console.log(`Server running on port 8081`);
-});
+//app2.listen(8081, () => {
+//  console.log(`Server running on port 8081`);
+//});
 
